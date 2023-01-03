5749f568bde0714c2b37167bbd4692dd3819ec2a
Uygar Suslu <uygarsuslu1@gmail.com>
2023-01-03 21:38:21 +0300

    svgr added

---
+++ stories/1-Button.stories.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Button from "../components/button"
+
+export default {
+    title: "Buttons"
+}
+
+
+export const Normal = () => (
+    <Button>
+      Save
+    </Button>
+)
