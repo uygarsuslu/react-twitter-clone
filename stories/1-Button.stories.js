2f3da0274ceb2bb7d77e8a97edf5c2692407a472
Uygar Suslu <uygarsuslu1@gmail.com>
2023-01-04 23:50:51 +0300

    navigation icons added

--- stories/1-Button.stories.js
+++ stories/1-Button.stories.js
@@ -1,6 +1,8 @@
 import React from "react";
 
 import Button from "../components/button"
+import NavButton from "../components/nav-button"
+import Navigation from "../components/navigation"
 
 export default {
     title: "Buttons"
@@ -11,4 +13,14 @@ export const Normal = () => (
     <Button>
       Save
     </Button>
+)
+
+export const NavigationButton = () => (
+  <NavButton>
+    Save
+  </NavButton>
+)
+
+export const Nav = () => (
+  <Navigation />
 )
