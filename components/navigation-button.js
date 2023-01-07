437e998018ff48c1c77eaeba1878390657bcf60e
Uygar Suslu <uygarsuslu1@gmail.com>
2023-01-07 17:13:08 +0300

    notification indicator added

--- components/navigation-button.js
+++ components/navigation-button.js
@@ -6,8 +6,13 @@ import Button from "./button"
 
 import styles from './navigation-button.module.css'
 
-function NavigationButton({selected, children, ...props}) {
-    return <Button className={cn(styles.navButton, selected && styles.navButtonSelected)} {...props}>{children}</Button>
+function NavigationButton({notify, selected, children, ...props}) {
+    return (
+        <Button className={cn(styles.navButton, selected && styles.navButtonSelected)} {...props}>
+            {children}
+            {notify && <span className={styles.notify}>{notify}</span>}
+        </Button>
+    )
 }
 
 export default NavigationButton
