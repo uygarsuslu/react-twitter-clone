e9ca5785ff15e3f4cd65bc299ad4ffd245bc91e5
Uygar Suslu <uygarsuslu1@gmail.com>
2023-01-11 09:48:27 +0300

    button components reworked

--- components/button.js
+++ components/button.js
@@ -4,8 +4,8 @@ import cn from "classnames"
 
 import styles from './button.module.css'
 
-function Button({children, className, ...props}) {
-    return <button type="button" className={cn(styles.button, className)} {...props}>{children}</button>
+function Button({full = false, children, className, ...props}) {
+    return <button type="button" className={cn(styles.button, full && styles.fullWidth, className)} {...props}>{children}</button>
 }
 
 export default Button
--- stories/1-Button.stories.js
+++ stories/1-Button.stories.js
@@ -5,16 +5,21 @@ import NavigationButton from "../components/navigation-button"
 import Navigation from "../components/navigation"
 import { Home } from "../components/icons";
 import TextTitle from "../components/text-title";
+import ThemeButton from "../components/theme-button";
 
 export default {
     title: "Buttons"
 }
 
 
-export const Normal = () => (
-    <Button>
-      Save
-    </Button>
+export const Normal = () => <Button>Save</Button>
+
+export const Theme = () => (
+  <div>
+    <ThemeButton>Save</ThemeButton>
+    <ThemeButton full>Save Full</ThemeButton>
+    <ThemeButton full big>Save Bit Button</ThemeButton>
+  </div>
 )
 
 export const NavButton = () => (
@@ -24,6 +29,4 @@ export const NavButton = () => (
   </NavigationButton>
 )
 
-export const Nav = () => (
-  <Navigation selectedKey="home" />
-)
+export const Nav = () => <Navigation selectedKey="home" />
