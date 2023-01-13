83421b4e73835deb8200300b35f1ed2d172c69f7
Uygar Suslu <uygarsuslu1@gmail.com>
2023-01-13 21:37:51 +0300

    useWindowSize hook added

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,19 @@
 import React from "react"
 
+import CONST from "../constants"
+
 import "../styles/app.css"
 import Layout from "../components/layout"
 
+import useWindowSize from "../hooks/useWindowSize";
+
 function HomePage() {
+    const size = useWindowSize();
     return (
         <Layout>
-          <div>sidebar</div>
+          <div flat={size.width < CONST.DESKTOP_SIZE}>sidebar</div>
           <div>main</div>
-          <div>extra</div>
+          {size.width > CONST.TABLET_SIZE && <div>extra</div>}
         </Layout>
     )
 }
