3a8ebcfa9dbf3ab121b2e8b9583218472aa1916c
Uygar Suslu <uygarsuslu1@gmail.com>
2023-01-16 22:39:29 +0300

    ınstalled postcss plugin for media-query

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ca87f4f6aff8a007"; }
+/******/ 		__webpack_require__.h = function() { return "7c56ea5e2197cff1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
