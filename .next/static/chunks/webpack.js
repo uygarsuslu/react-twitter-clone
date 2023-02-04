3645548376a912569cb08e9e8e0f1082cc49741e
Uygar Suslu <uygarsuslu1@gmail.com>
2023-02-04 12:48:53 +0300

    added size property for photo component

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3bc5dea8b489784c"; }
+/******/ 		__webpack_require__.h = function() { return "858d2abac1217e1e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/photo.js
+++ components/photo.js
@@ -4,9 +4,9 @@ import cn from "classnames"
 
 import styles from "./photo.module.css"
 
-function Photo ({ src="https://pbs.twimg.com/profile_images/1582037563149324295/IGLvWd2y_400x400.jpg", alt }) {
+function Photo ({ src="https://pbs.twimg.com/profile_images/1582037563149324295/IGLvWd2y_400x400.jpg", alt, size= 47 }) {
     return (
-        <div className={cn([styles.photo])} >
+        <div className={cn([styles.photo])} style={{width:size, height:size}} >
             <img className={styles.img} src={src} alt={alt} />
         </div>
     )
