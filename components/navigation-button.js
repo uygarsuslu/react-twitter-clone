941f1c061d34c3e7fd7723559bce2a968e17f960
Uygar Suslu <uygarsuslu1@gmail.com>
2023-02-13 18:19:46 +0300

    navigation refactored

--- components/navigation-button.js
+++ components/navigation-button.js
@@ -1,14 +1,13 @@
 import React from "react"
-
 import cn from "classnames"
 
 import Button from "./button"
 
 import styles from './navigation-button.module.css'
 
-function NavigationButton({notify, selected, children, ...props}) {
+function NavigationButton({notify, selected, children, className, ...props}) {
     return (
-        <Button className={cn(styles.navButton, selected && styles.navButtonSelected)} {...props}>
+        <Button className={cn(styles.navButton, selected && styles.navButtonSelected, className)} {...props}>
             {notify > 0 && <span className={styles.notify}>{notify}</span>}
             {children}
         </Button>
--- components/navigation.js
+++ components/navigation.js
@@ -1,87 +1,31 @@
 import React from "react"
+import { useRouter } from 'next/router'
 
+import { MENU } from "../constants"
 import styles from './navigation.module.css'
 
 import NavigationButton from "./navigation-button"
 import TextTitle  from "./text-title"
 
-import {Twitter, Home, Explore, Notification, Messages, Bookmark, Lists, Profile, More, HomeFill, ExploreFill, NotificationFill, MessagesFill, BookmarkFill, ListsFill, ProfileFill } from "./icons"
+function Navigation({flat = false}) {
+    const router = useRouter()
 
-const MENU = [
-    {
-        key: "twitter",
-        icon: <Twitter />,
-        iconSelected: <Twitter />,
-        title: "",
-        notify: 0
-    }, 
-    {
-        key: "home",
-        icon: <Home />,
-        iconSelected: <HomeFill />,
-        title: "Home",
-        notify: 0
-    },
-    {
-        key: "explore",
-        icon: <Explore />,
-        iconSelected: <ExploreFill />,
-        title: "Explore",
-        notify: 0
-    },
-    {
-        key: "notification",
-        icon: <Notification />,
-        iconSelected: <NotificationFill />,
-        title: "Notification",
-        notify: 17
-    },
-    {
-        key: "messages",
-        icon: <Messages />,
-        iconSelected: <MessagesFill />,
-        title: "Messages",
-        notify: 0
-    },
-    {
-        key: "bookmark",
-        icon: <Bookmark />,
-        iconSelected: <BookmarkFill />,
-        title: "Bookmark",
-        notify: 0
-    },
-    {
-        key: "lists",
-        icon: <Lists />,
-        iconSelected: <ListsFill />,
-        title: "Lists",
-        notify: 0
-    },
-    {
-        key: "profile",
-        icon: <Profile />,
-        iconSelected: <ProfileFill />,
-        title: "Profile",
-        notify: 0
-    },
-    {
-        key: "more",
-        icon: <More />,
-        iconSelected: <More />,
-        title: "More",
-        notify: 0
-    },
-]
-
-function Navigation({flat = false, selectedKey = "messages"}) {
     return (
         <nav className={styles.nav}>
             {
                 MENU.map((menu) => {
+
                     const showTitle = !flat && menu.title.length > 0
-                    const selected = selectedKey === menu.key
+                    const selected = router.pathname === menu.path
+                    
                     return(
-                        <NavigationButton key={menu.key} notify={menu.notify} selected={selected}>
+                        <NavigationButton 
+                           key={menu.key} 
+                           notify={menu.notify} 
+                           selected={selected} 
+                           href={menu.path} 
+                           className={styles.navButton}
+                        >
                             {selected ? menu.iconSelected : menu.icon}
                             {showTitle && <TextTitle>{menu.title}</TextTitle>}
                         </NavigationButton>
