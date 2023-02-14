45315d2be1ec554657eea6434a49411e254c35af
Uygar Suslu <uygarsuslu1@gmail.com>
2023-02-14 11:38:16 +0300

    added option as link or button for button component

--- components/button.js
+++ components/button.js
@@ -1,11 +1,33 @@
 import React from "react"
-
 import cn from "classnames"
+import Link from "next/link"
 
 import styles from './button.module.css'
 
+function LinkButton({href, children, ...props}) {
+    return (
+        <Link legacyBehavior href={href}>
+            <a {...props}>{children}</a>
+        </Link>
+    )
+}
+
+function BaseButton({children, ...props}) {
+    return (
+        <button type="button" {...props}>
+            {children}
+        </button>
+    )
+}
+
 function Button({full = false, children, className, ...props}) {
-    return <button type="button" className={cn(styles.button, full && styles.fullWidth, className)} {...props}>{children}</button>
+    const Comp = props.href ? LinkButton : BaseButton
+
+    return (
+        <Comp className={cn(styles.button, full && styles.fullWidth, className)} {...props}>
+            {children}
+        </Comp>
+    )
 }
 
 export default Button
