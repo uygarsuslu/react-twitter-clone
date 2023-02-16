210bba5abff4041a29af91568a035fa8cd0f4b2d
Uygar Suslu <uygarsuslu1@gmail.com>
2023-02-16 19:43:58 +0300

    columns are included in the layout

--- components/layout.js
+++ components/layout.js
@@ -1,11 +1,25 @@
 import React from "react"
-
 import cn from "classnames"
+
+import useWindowSize from "../hooks/useWindowSize";
 
 import styles from "./layout.module.css"
 
+import CONST from "../constants"
+import Sidebar from "../components/col-sidebar";
+import Main from "../components/col-main";
+import Extra from "../components/col-extra";
+
 function Layout ({ children }) {
-    return  <div className={cn(styles.layout)}>{children}</div>
+    const size = useWindowSize();
+
+    return  <div className={cn(styles.layout)}>
+        <Sidebar flat={size.width < CONST.DESKTOP_SIZE}>sidebar</Sidebar>
+
+        <Main>{children}</Main>
+
+        {size.width > CONST.TABLET_SIZE && <Extra>extra</Extra>}
+    </div>
 }
 
 export default Layout
