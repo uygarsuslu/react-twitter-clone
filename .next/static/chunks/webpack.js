417f7a55a05cdb87beb4fc15b8ebd67b2b216886
Uygar Suslu <uygarsuslu1@gmail.com>
2023-02-21 16:33:51 +0300

    sidebar ui fix

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9fd5a4f802ee8178"; }
+/******/ 		__webpack_require__.h = function() { return "330b77c4759a19f0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
